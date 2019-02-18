79c317e3a01d6351e534a04bd7042a3b777fef96
hectorgh <hectorgh@gmail.com>
2019-02-18 10:30:35 +0100

    adding source files

---
+++ install/usr/share/n4d/python-plugins/AutofsManager.py
@@ -0,0 +1,79 @@
+import os
+import tempfile
+import re
+
+
+class AutofsManager:
+	
+	def __init__(self):
+		
+		self.auto_master_dir="/etc/auto.master.d/"
+		self.auto_master_options="--timeout 60"
+		
+		self.default_options="-rw,hard,intr,rsize=8192,wsize=8192,nosuid,nfsvers=3"
+		self.default_dest="/net/server-sync/&"
+				
+		#TESTING VALUES
+		#self.auto_master_dir="/tmp/auto.master.d/"
+		
+	#def init
+	
+	
+	def startup(self,options):
+		
+		pass
+		
+	#def startup
+	
+	
+	def create_master_file(self,mount_dest,mount_script,options=None):
+		
+		if not os.path.exists(self.auto_master_dir):
+			os.makedirs(self.auto_master_dir)
+		
+		if options==None:
+			options=self.auto_master_options
+		
+		mount_dest=mount_dest.rstrip("/")
+		dest_path=self.auto_master_dir+mount_dest.lstrip("/").rstrip("/").replace("/","_")+".autofs"
+		data="%s\t%s\t%s\n"%(mount_dest,mount_script,options)
+		
+		fd, tmpfilepath = tempfile.mkstemp()
+		new_export_file = open(tmpfilepath,'w')
+		new_export_file.write(data)
+		new_export_file.close()
+		os.close(fd)
+		
+		os.rename(tmpfilepath,dest_path)
+		
+		
+	#def create_master_file
+	
+	
+	def create_mount_script(self,mount_script_fname,subdir_dest,mount_source,options=None):
+		
+		if options==None:
+			options=self.default_options
+			
+		data="%s\t%s\t%s\n"%(subdir_dest,options,mount_source)
+		
+		fd, tmpfilepath = tempfile.mkstemp()
+		new_export_file = open(tmpfilepath,'w')
+		new_export_file.write(data)
+		new_export_file.close()
+		os.close(fd)
+		
+		os.rename(tmpfilepath,mount_script_fname)
+		
+	#def create_mount_script
+	
+	
+#class AutofsManager
+
+if __name__=="__main__":
+	
+	autofs=AutofsManager()
+	
+	autofs.create_master_file("/net/server-sync/","/etc/auto.nfs")
+	autofs.create_mount_script("/tmp/auto.master.d/wawa","*","172.20.10.19:/net/server-sync/&")
+	
